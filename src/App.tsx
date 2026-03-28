c30bcfb503e84c42145d3dec26a40db0c2225891
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-03-29 04:10:07 +0530

    App.jsx integration done only stylign left

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { useCallback } from "react";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { LogicNodeTree } from "./components/LogicNode";
+import { LinkNodeModal } from "./components/LinkNodeModal";
+import { useStore } from "./store/useStore";
 
-const App = () => {
+function App() {
+  const rootId = useStore((s) => s.rootId);
+  const setSelectedNode = useStore((s) => s.setSelectedNode);
+  const setLinkingFrom = useStore((s) => s.setLinkingFrom);
+  const linkingFromId = useStore((s) => s.linkingFromId);
+
+  const handleCanvasClick = useCallback(() => {
+    setSelectedNode(null);
+    if (linkingFromId) setLinkingFrom(null);
+  }, [setSelectedNode, setLinkingFrom, linkingFromId]);
+
   return (
-    <div>
-      
+    <div className="app">
+      <Header />
+
+      <main className="canvas-wrapper" onClick={handleCanvasClick}>
+        <div className="canvas-inner">
+          <LogicNodeTree nodeId={rootId} isRoot />
+        </div>
+      </main>
+
+      <Sidebar />
+      <LinkNodeModal />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
